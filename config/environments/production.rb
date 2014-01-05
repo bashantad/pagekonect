517bbd7b2a4f29064fd55f0297983c23f1735017
bashantad <dahal_bsd@yahoo.com>
2014-01-05 19:56:35 +1100

    precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ Pagekonect::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 end

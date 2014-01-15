49bbebde44ca424643e6dadf7b8f42ca1f818cb5
Pratuat <pratuat@gmail.com>
2014-01-16 02:04:43 +0545

    votable implemented for content, buttons added to news index and show page

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -4,15 +4,21 @@ class ContentsController < ApplicationController
 
   def vote_up
     content.liked_by current_user
+    respond_to do |format|
+      format.js { render 'vote.coffee' }
+    end
   end
 
   def vote_down
-    content.unliked_by current_user
+    content.unliked_by voter: current_user
+    respond_to do |format|
+      format.js { render 'vote.coffee' }
+    end
   end
 
   private
 
     def content
-      Content.find(params[:id])
+      @content = Content.find(params[:id])
     end
 end

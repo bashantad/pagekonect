6e463f718ddde8154beea768be7c7173d8cd56cb
Lujaw <lujaw@hotmail.com>
2014-01-28 23:19:45 +0545

    validation implemented

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,4 @@
 class Video < Content
-  validates :url, :presence => true
+  require 'uri'
+  validates :url, :presence => true, format: { with: /(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?/ }
 end

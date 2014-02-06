4ce812f32370ed01819c96e0f84b5f37cbe50f5a
bashantad <dahal_bsd@yahoo.com>
2014-02-06 21:38:58 +1100

    improved css and added home icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,18 @@ module ApplicationHelper
   def class_name(object)
     object.try(:is_content?) ? "Content" : object.class.name 
   end
+  
+  def get_create_link_with_msg(controller)
+    case controller
+    when "news"
+       [new_news_path, "Create new newsfeed"]
+    when "deals"
+       [new_deal_path, "Create new deal"]
+    when "videos"
+       [new_video_path, "Create new video"]
+    else
+       [new_event_path, "Create new event or suggest one"]
+    end
+  end
 end
+

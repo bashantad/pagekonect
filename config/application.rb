5794deb225a3c37ee6f32770f7f4fc5cfc23060f
bashantad <dahal_bsd@yahoo.com>
2014-05-30 20:12:36 +1000

    serve static assets

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Pagekonect
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.initialize_on_precompile = false
+    config.serve_static_assets = true
   end
 end

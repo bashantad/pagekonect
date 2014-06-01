eab032a23016e35d9e371cefa3f998dd1c47cfcb
bashantad <dahal_bsd@yahoo.com>
2014-06-01 20:11:23 +1000

    sorted result on based on created date

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
 
   def index
     if query = params[:query] && (search_index = PgSearch.multisearch(params[:query])).present?
-      @search_results = search_index.map(&:searchable)
+      @search_results = search_index.map(&:searchable).sort{|a,b| b.created_at <=> a.created_at}
       @title_length = 83
       @iframe_width = 230
       @iframe_height = 150
